7915f597b2600b186de1c4fb6de781b9ef483709
iamwendellbalagot <johnwendellbalagot11111@gmail.com>
2021-01-07 00:11:08 +0800

    Added Update DOM

--- Raku.js
+++ Raku.js
@@ -28,27 +28,51 @@ const createDOM = (fiber) => {
     fiber.type === "plainText"
       ? document.createTextNode("")
       : document.createElement(fiber.type);
-  Object.keys(fiber.props)
-    .filter((key) => key !== "children")
-    .map((c) => (node[c] = fiber.props[c]));
+  updateDOM(node, {}, fiber.props);
   //[Not appending the child elemnts to the node]
   //fiber.props.children.map(child => render(child, node))
   return node;
 };
 
-const isProp = key => key !== 'children';
-const destroyProps = (prev, next) => key => !(key in next);
-const newProps = (prev, next) => key => prev[key] !== next[key];
+const isEvent = (key) => key.startsWith("on");
+const isProp = (key) => key !== "children" && !isEvent(key);
+const destroyProps = (prev, next) => (key) => !(key in next);
+const newProps = (prev, next) => (key) => prev[key] !== next[key];
 const updateDOM = (dom, prevProps, nextProps) => {
-  Object.keys(prevProps).filter(isProp).filter(destroyProps)
-    .map(prop => dom[prop] = '');
-  Object.keys(nextProps).filter(isProp).filter(newProps)
-    .map(prop => dom[prop] = nextProps[prop]);
-}
+  console.log("Update DOM: ", dom);
+  Object.keys(prevProps)
+    .filter(isEvent)
+    .filter((key) => !(key in nextProps) || newProps(prevProps, nextProps)(key))
+    .forEach((ev) => {
+      const eventType = ev.toLowerCase().substring(2);
+      dom.removeEventListener(eventType, prevProps[ev]);
+    });
+
+  Object.keys(prevProps)
+    .filter(isProp)
+    .filter(destroyProps(prevProps, nextProps))
+    .forEach((prop) => {
+      dom[prop] = "";
+    });
+  Object.keys(nextProps)
+    .filter(isProp)
+    .filter(newProps(prevProps, nextProps))
+    .forEach((prop) => {
+      dom[prop] = nextProps[prop];
+    });
+  Object.keys(nextProps)
+    .filter(isEvent)
+    .filter(newProps(prevProps, nextProps))
+    .forEach((ev) => {
+      const eventType = ev.toLowerCase().substring(2);
+      console.log("EventType: ", eventType);
+      dom.addEventListener(eventType, nextProps[ev]);
+    });
+};
 
 const commitRoot = () => {
   //Commit the node into the dom
-  deletes.forEach(commitWork)
+  deletes.forEach(commitWork);
   commitWork(wipRoot.child);
   rootRef = wipRoot;
   wipRoot = null;
@@ -56,13 +80,17 @@ const commitRoot = () => {
 };
 
 const commitWork = (fiber) => {
-  if (!fiber) return;
+  if (!fiber) {
+    return;
+  }
   const domParent = fiber.parent.dom;
-  (fiber.effectTag === 'CREATE' && fiber.dom) 
-    && domParent.appendChild(fiber.dom);
-  (fiber.effectTag === 'DESTROY') && domParent.removeChild(fiber.dom);
-  (fiber.effectTag === 'UPDATE' && fiber.dom)
-    && (updateDOM(fiber.dom, fiber.alternate.props, fiber.props)); 
+  if (fiber.effectTag === "CREATE" && fiber.dom != null) {
+    domParent.appendChild(fiber.dom);
+  } else if (fiber.effectTag === "UPDATE" && fiber.dom != null) {
+    updateDOM(fiber.dom, fiber.alternate.props, fiber.props);
+  } else if (fiber.effectTag === "DESTROY") {
+    domParent.removeChild(fiber.dom);
+  }
   commitWork(fiber.child);
   commitWork(fiber.sibling);
 };
@@ -90,76 +118,98 @@ const workLoadLoop = (deadline) => {
     yieldStatus = deadline.timeRemaining() < 1;
     console.log("Next LOAD: ", yieldStatus, nextWorkLoad);
   }
-  !nextWorkLoad && wipRoot && commitRoot();
+  if (!nextWorkLoad && wipRoot) commitRoot();
   requestIdleCallback(workLoadLoop);
 };
 
 requestIdleCallback(workLoadLoop);
 
 const performWorkLoad = (fiber) => {
-  !fiber.dom && (fiber.dom = createDOM(fiber));
+  if (!fiber.dom) {
+    fiber.dom = createDOM(fiber);
+  }
 
   const elements = fiber.props.children;
   reconcileChildren(fiber, elements);
-  if (fiber.child) return fiber.child;
+  if (fiber.child) {
+    return fiber.child;
+  }
   let nextFiber = fiber;
   while (nextFiber) {
-    if (nextFiber.sibling) return nextFiber.sibling;
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
     nextFiber = nextFiber.parent;
   }
 };
-const reconcileChildren = (fiber, elements) => {
+const reconcileChildren = (wipFiber, elements) => {
   let index = 0;
-  let oldFiber = fiber.alternate && fiber.alternate.child;
+  let oldFiber = wipFiber.alternate && wipFiber.alternate.child;
+  console.log("OLDFIBER: ", oldFiber);
   let prevSibling = null;
-  while (index < elements.length || oldFiber) {
-    let element = elements[index];
+  while (index < elements.length || oldFiber != null) {
+    const element = elements[index];
     let newFiber = null;
-    const sameType = oldFiber && element && element.type === oldFiber.type;
+    const sameType = oldFiber && element && element.type == oldFiber.type;
 
-    sameType &&
-      (newFiber = {
+    if (sameType) {
+      newFiber = {
         type: oldFiber.type,
         props: element.props,
         dom: oldFiber.dom,
-        parent: fiber,
+        parent: wipFiber,
         alternate: oldFiber,
         effectTag: "UPDATE"
-      });
+      };
+    }
 
-    !sameType &&
-      element &&
-      (newFiber = {
+    (!sameType && element) && (
+      newFiber = {
         type: element.type,
         props: element.props,
         dom: null,
         alternate: null,
-        parent: fiber,
-        effectTag: 'CREATE'
-      });
-
-    (!sameType && oldFiber) && (
-      oldFiber.effectTag = 'DESTROY',
-      deletes.push(oldFiber)
+        parent: wipFiber,
+        effectTag: "CREATE"
+      }
     );
 
-    index === 0 && (fiber.child = newFiber);
-    index !== 0 && (prevSibling.sibling = newFiber);
+    if (!sameType && oldFiber) {
+      oldFiber.effectTag = "DESTROY";
+      deletes.push(oldFiber);
+    }
+
+    if (oldFiber) {
+      oldFiber = oldFiber.sibling
+    }
+
+    if (index === 0) {
+      wipFiber.child = newFiber;
+    } else if (element) {
+      prevSibling.sibling = newFiber;
+    }
     prevSibling = newFiber;
     index++;
   }
 };
 
-/** @jsx createElement */
+const Raku = { createElement, render };
+/** @jsx Raku.createElement */
 
 const container = document.getElementById("root");
 
-let el = (
-  <div id="test">
-    <h1>Welcome</h1>
-    <div>Hello Worldd</div>
-    <input />
-  </div>
-);
+const updateValue = (e) => {
+  rerender(e.target.value);
+};
 
-render(el, container);
+const rerender = (value) => {
+  const element = (
+    <div>
+      <input onInput={updateValue} value={value} />
+      <h2>Hello {value}</h2>
+    </div>
+  );
+  Raku.render(element, container);
+};
+
+rerender("World");
