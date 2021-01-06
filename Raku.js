688e35a2c8cbd11c1acc2885c6d7e71d5d93f309
iamwendellbalagot <johnwendellbalagot11111@gmail.com>
2021-01-06 20:07:39 +0800

    Added fiber creation

--- Raku.js
+++ Raku.js
@@ -1,3 +1,14 @@
+
+// const render = (element, container) => {
+//   const node = element.type === 'plainText' ? document.createTextNode('')
+//     : document.createElement(element.type)
+//   Object.keys(element.props ).filter(key => key !== 'children')
+//     .map(c => node[c] = element.props[c])
+//   element.props.children.map(child => render(child, node))
+//   container.appendChild(node);
+//   console.log(element)
+// }
+
 const createElement = (type, props, ...children) => (
   {type, props: {...props, children: children.map( child =>
     typeof child !== 'object'? createTextElement(child) : child  
@@ -11,13 +22,64 @@ const createTextElement = (text) => (
   }
 );
 
+const createDOM = (fiber) => {
+  const node = fiber.type === 'plainText' ? document.createTextNode('')
+    : document.createElement(fiber.type)
+  Object.keys(fiber.props ).filter(key => key !== 'children')
+    .map(c => node[c] = fiber.props[c])
+  //[Not appending the child elemnts to the node]  
+    //fiber.props.children.map(child => render(child, node))
+  return node;
+}
+
 const render = (element, container) => {
-  const node = element.type === 'plainText' ? document.createTextNode('')
-    : document.createElement(element.type)
-  Object.keys(element.props ).filter(key => key !== 'children')
-    .map(c => node[c] = element.props[c])
-  element.props.children.map(child => render(child, node))
-  container.appendChild(node);
+  nextWorkLoad = {
+    dom: container,
+    props: {
+      children: [element]
+    }
+  }
+}
+
+let nextWorkLoad = null;
+const workLoadLoop = (deadline) => {
+  let yieldStatus = false;
+  while(nextWorkLoad && !yieldStatus){
+    nextWorkLoad = performWorkLoad(nextWorkLoad)
+    yieldStatus = deadline.timeRemaining() < 1;
+    console.log('Next LOAD: ',nextWorkLoad)
+  }
+  requestIdleCallback(workLoadLoop)
+}
+
+requestIdleCallback(workLoadLoop)
+
+const performWorkLoad = (fiber) => {
+  !fiber.dom && (fiber.dom = createDOM(fiber));
+  fiber.parent && (fiber.parent.dom.appendChild(fiber.dom));
+
+  const elements = fiber.props.children;
+  let index = 0;
+  let prevSibling = null;
+  while(index < elements.length){
+    let element = elements[index];
+    let newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null
+    };
+    index === 0 && (fiber.child = newFiber);
+    index !== 0 && (prevSibling.sibling = newFiber);
+    prevSibling = newFiber;
+    index++;
+  }
+  if(fiber.child) return fiber.child;
+  let nextFiber = fiber
+  while(nextFiber){
+    if(nextFiber.sibling) return nextFiber.sibling
+    nextFiber = nextFiber.parent
+  }
 }
 
 /** @jsx createElement */
